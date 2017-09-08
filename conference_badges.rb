c5c5da68bf698cb7fcdc951f5822bccf96494203
Colin McKean <ezikill@hotmail.com>
2017-09-08 10:29:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator([attendees])
+def batch_badge_creator(attendees)
   attendees.each{|e| badge_maker(e)}
   return "woof"
 end
