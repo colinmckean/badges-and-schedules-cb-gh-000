b9b1050708128b7e4877966db25460781e995663
Colin McKean <ezikill@hotmail.com>
2017-09-08 10:36:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,10 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+def assign_rooms(name)
+  "Hello, #{name}! You'll be assigned to room 1!"
+end
+
 def batch_badge_creator(attendees)
   batch = []
   attendees.each{|e| batch.push( badge_maker(e))}
